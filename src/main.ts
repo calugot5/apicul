b98b97a0d44d2cea91d833e0031658c5d093ff17
calugot5 <116294346+calugot5@users.noreply.github.com>
2024-05-06 22:24:19 -0500

    Class-validator en el recurso user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Agregamos validador automático a nuestra app
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
